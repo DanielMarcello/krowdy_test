ac030aa216100610d2a2c487bde2dba04419db8c
Daniel Salinas <daniel.salinas.tellez@gmail.com>
2018-12-31 12:20:43 -0500

    Adds footer

--- krowdy/src/components/App.js
+++ krowdy/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Header from "./global/Header";
+import Footer from "./global/Footer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Footer />
       </div>
     );
   }
---
+++ krowdy/src/components/global/Content.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import "./css/Content.css";
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="Content">
+        <p>Krowdy Corporation &copy; 2018</p>
+      </div>
+    );
+  }
+}
+
+export default Content;
